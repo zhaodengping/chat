47aab736e642b830e51e7ea669cd1d3387083a1b
Alice Zhao <alicezhao@SHA-MC0326.local>
2020-03-31 16:43:48 +0800

    给服务器发消息，返回success

--- back/chat.js
+++ back/chat.js
@@ -4,6 +4,7 @@ console.log("建立连接")
 let server=ws.createServer(function(conn){
     conn.on("text",function(str){
         conn.sendText("success")
+        console.log(str)
     });
     conn.on("close",function(str){
         console.log('服务器关闭')
--- front/chat/src/App.vue
+++ front/chat/src/App.vue
@@ -13,6 +13,17 @@ export default {
       let ws=new WebSocket("ws://localhost:8081");
       ws.onopen=function(){
         console.log('服务器连接成功')
+        ws.send("页面1服务器发消息了")
+      }
+      
+      ws.onmessage=function(e){
+        console.log(e)
+      }
+      ws.onclose=function(){
+         console.log('连接关闭')
+      }
+      ws.onerror=function(){
+        console.log('发生错误')
       }
     }else{
       console.log('浏览器暂不支持websocket')
