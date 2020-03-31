cb7fa9fe59982d825d6de0010cc72b0989dad89f
Alice Zhao <alicezhao@SHA-MC0326.local>
2020-03-31 18:22:21 +0800

    简单的聊天室 机器人和我之间的对话

--- back/chat.js
+++ back/chat.js
@@ -1,10 +1,17 @@
 let ws=require('nodejs-websocket');
 
 console.log("建立连接")
+let isName=true
 let server=ws.createServer(function(conn){
+    console.log(isName)
     conn.on("text",function(str){
-        conn.sendText("success")
         console.log(str)
+        if(isName){
+            conn.sendText(str+'上线了')
+            isName=false
+        }else{
+            conn.sendText(str)
+        }
     });
     conn.on("close",function(str){
         console.log('服务器关闭')
